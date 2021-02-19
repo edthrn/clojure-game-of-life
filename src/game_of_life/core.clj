6d59cf08a404ab0372f14bf99d9e7d67ff8d9df7
Edouard Theron <ed@edtheron.me>
2021-02-18 18:28:59 -0600

    Use 'into' instead of 'vec'
    Fix some styling.

--- src/game_of_life/core.clj
+++ src/game_of_life/core.clj
@@ -2,34 +2,24 @@
   (:require [clojure.string :as string])
   (:gen-class))
 
-(def random (java.util.Random.))
-(def width 50)
-(def height 50)
-(def refresh-rate 2)  ;; 2 frames/sec
-
-;; A cell is just a boolean value for now.
 (def alive true)
 (def dead false)
-
 (defn alive? [cell] cell)
+(def width 100)
+(def height 50)
+(def refresh-rate 24)  ;; 24 frames/sec
 
 (defn get-cell-repr [cell] (if (alive? cell) "⬛" "⬜"))
 
 (defn get-grid-repr
   "Get a string representation of the given grid."
   [grid]
-  (string/join
-   "\n"
-   (for
-    [y (range height)]
-    (string/join
-     (for
-      [x (range width)]
-      (get-cell-repr (get-in grid [x y])))))))
+  (string/join "\n" (for [y (range height)]
+                         (string/join (for [x (range width)]
+                                           (get-cell-repr
+                                            (get-in grid [x y])))))))
 
 (defn display [grid] (println (get-grid-repr grid) "\n"))
-
-(defn generate-booleans [n] (repeatedly n #(. random nextBoolean)))
 
 (defn count-alive-cells [cells] (reduce + (for [c cells :when (alive? c)] 1)))
 
@@ -37,17 +27,16 @@
   "Get all cells in given cell's Moore neighborhood, excluding cell itself."
   [grid x y]
   (let [steps [-1 0 1]]
-    (for
-     [dx steps dy steps :when (not (= [dx dy] [0 0]))] ;; exclude cell itself
-     (get-in grid [(+ x dx) (+ y dy)]))))
+   (for [dx steps dy steps :when (not (= [dx dy] [0 0]))] ;; exclude this cell
+        (get-in grid [(+ x dx) (+ y dy)]))))
 
 (defn get-alive-neighbors-grid
   "Given a grid, return a matrix counting alive neighbors for each cell."
   [grid]
-  (vec (for [x (range width)] (vec (for [y (range height)]
-                                        (count-alive-cells
-                                         (get-cells-in-moore-neighborhood
-                                          grid x y)))))))
+  (into [] (for [x (range width)]
+                (into [] (for [y (range height)]
+                              (count-alive-cells
+                               (get-cells-in-moore-neighborhood grid x y)))))))
 
 (defn get-cell-evolution
   "Get the next cell's state, either dead or alive."
@@ -64,16 +53,19 @@
   If an existing grid is given, returns the evolution based on Conway's rules.
   Otherwise, returns a randomly created grid.
   "
-  ([] (vec (repeatedly width #(vec (generate-booleans height)))))
+  ([]
+  (into [] (repeatedly
+            width
+            (fn [] (into [] (repeatedly
+                             height
+                             (fn [] (rand-nth [alive dead]))))))))
   ([previous-grid]
   (let [alive-neighbors-grid (get-alive-neighbors-grid previous-grid)]
-    (vec (for
-          [x (range width)]
-          (vec (for
-                [y (range height)]
-                (get-cell-evolution
-                  (get-in previous-grid [x y])
-                  (get-in alive-neighbors-grid [x y])))))))))
+    (into [] (for [x (range width)]
+                  (into [] (for [y (range height)]
+                                (get-cell-evolution
+                                 (get-in previous-grid [x y])
+                                 (get-in alive-neighbors-grid [x y])))))))))
 
 (defn play
   [grid]
@@ -82,3 +74,4 @@
   (get-next-grid grid))
 
 (defn -main [] (doall (iterate play (get-next-grid))))
+
